060ee83cf370e272ebf58515f999fb9294258ca0
Cindy Kei <myk245@nyu.edu>
2019-12-03 02:09:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,6 +27,8 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-  game_hash.each do 
+def num_points_scored(name)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player_name, player_data|
+      
 end
