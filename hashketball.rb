bc5e8a904986415c0d976bd8968a898a980ca407
Cindy Kei <myk245@nyu.edu>
2019-12-03 02:10:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,4 +32,9 @@ def num_points_scored(name)
     team_data[:players].each do |player_name, player_data|
       if player_name == name
         return player_data[:points]
+      elsif player_name =! name
+  return nil
+  end
+end
+end
 end
