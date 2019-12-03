bd9f0e670b4703df1272da391e3e839ab6ae8804
Cindy Kei <myk245@nyu.edu>
2019-12-03 03:08:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,6 +69,7 @@ def player_stats(players_name)
       if player_stats.has_value?(players_name)
         players_stats.delete(:player_name)
         return player_stats
+      end 
     end
   end
 end
